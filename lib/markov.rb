32e0b018b043a42931a2cbbaec88115fdb26a1c2
Erik Jönsson <efjonsson@gmail.com>
2016-10-26 17:52:47 -0400

    generate sentences based on word pairs

--- lib/markov.rb
+++ lib/markov.rb
@@ -1,38 +1,17 @@
 require 'byebug'
 require './lib/word_calculator'
 
-# Step 1:
-# Given a text file including a entire book, find:
-# - most frequently used words
-# - how many different words there are
-#
-# Given this information, log a sentence stating:
-# "The top three frequent words used in [filename] out of [number] different
-# words are [word1], [word2], [word3]."
-
 TEST = "I love bananas. I love apples. So fun to love those things.".freeze
 REDDIT_DATA_FILE = "./lib/reddit_comments_raw.csv"
 
-REDDIT_COMMENTS = IO.read(REDDIT_DATA_FILE, 1000 * 1000)
+REDDIT_COMMENTS = IO.read(REDDIT_DATA_FILE, 10000 * 10000)
 
 word_calculator = WordCalculator.new(REDDIT_COMMENTS)
 total_words = word_calculator.total_words
 top_3_words = word_calculator.top_words(3)
-# puts "The top three frequent words used in #{Reddit_Data_file} out of " \
-#      "#{total_words} different words are #{top_3_words[0]}, " \
-#      "#{top_3_words[1]}, #{top_3_words[2]}."
 
-# Step 2:
-# Given a word, find all the "next word options". By "next word options" we mean
-# an array of all the words that follow the input word in the text. For example,
-# if the text read "I love bananas. I love apples. So fun to love those things."
-# and I asked to find the "next word options" for the word "love"... the return
-# should be [ "bananas", "apples", "those" ] as those words follow the word
-# "love" in the text.
+words = word_calculator.words
 
-# Get all the words
-words = word_calculator.words # => Array of words ["erik", "banana"]
-# Create a hash with each word and the words that follow it.
 word_pairs = {}
 words.each_index do |index|
   first_word = words[index]
@@ -44,7 +23,18 @@ words.each_index do |index|
   end
 end
 
-puts word_pairs
+# Select a word at random from top words
+initial_word = top_3_words.sample
+# For a specified number of words
+sentence = "#{initial_word}"
+20.times do
+  next_word = word_pairs[initial_word].sample
+  sentence += " #{next_word}"
+  initial_word = next_word
+end
+
+puts sentence
+# Select a word at random that follows the previous word
 
 
 # Bonus:
