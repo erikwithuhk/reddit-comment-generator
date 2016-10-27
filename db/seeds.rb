4df63a0f3c935710383f929f8bce3b91b55c2e96
Erik Jönsson <efjonsson@gmail.com>
2016-10-27 17:23:38 -0400

    rewrite text analyisis to handle CSV line-by-line

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,31 @@
+require 'csv'
 require './lib/word_calculator'
 
-TEST = "./lib/test.txt"
+TEST = "./lib/test.csv"
 REDDIT_DATA_FILE = "./lib/reddit_comments_raw.csv"
+#
+# REDDIT_COMMENTS = IO.read(REDDIT_DATA_FILE, 10000)
+#
+word_calculator = WordCalculator.new
+#
+# markov_data = word_calculator.markov_data
 
-REDDIT_COMMENTS = IO.read(TEST)
+# markov_data.each do |word, data|
+# end
+#
+#
 
-word_calculator = WordCalculator.new(REDDIT_COMMENTS)
+row_count = 0
+CSV.foreach(REDDIT_DATA_FILE) do |row|
+  if row_count > 6
+    break
+  else
+    word_calculator.analyze_line(row[0])
+    row_count += 1
+  end
+end
 
-markov_data = word_calculator.markov_data
-
-
-markov_data.each do |word, data|
-  # puts "Word: #{word}, Frequency: #{data[:frequency]}, Next words: #{data[:next_words]}"
-  Word.create(word: word, frequency: data[:frequency], next_words: data[:next_words])
+word_calculator.words.each do |word, data|
+  puts "Word: #{word}, Count: #{data[:count]}, Next Words: #{data[:next_words]}"
+  # Word.create(word: word, frequency: data[:frequency], next_words: data[:next_words])
 end
--- lib/word_calculator.rb
+++ lib/word_calculator.rb
@@ -1,74 +1,27 @@
 class WordCalculator
-  attr_reader :words, :total_words
+  attr_reader :words
 
-  def initialize(text)
-    @words = get_words(text)
-    @total_words = count_words
+  def initialize
+    @words = {}
   end
 
-  def top_words(limit)
-    word_frequencies = calculate_word_frequencies
-    sorted_word_frequencies = sort_by_frequency(word_frequencies)
-    top_frequencies = sorted_word_frequencies[0...limit]
-    combined_frequencies = top_frequencies.reduce(top_frequencies[0], :merge)
-    combined_frequencies.keys
-  end
-
-  def markov_data
-    word_frequencies = calculate_word_frequencies
-    word_data = {}
-    @words.each_index do |index|
-      first_word = words[index]
-      first_word_frequency = word_frequencies[first_word]
-      second_word = words[index + 1] || ""
-      if !word_data.include?(first_word)
-        word_data[first_word] = {
-          frequency: first_word_frequency,
-          next_words: ["#{second_word}"]
-        }
-      else
-        word_data[first_word][:next_words].push(second_word)
-      end
-    end
-    word_data
+  def analyze_line(line)
+    words = line.split(" ")
+    add_words(words)
   end
 
   private
 
-  attr_reader :word_frequencies
-
-  def calculate_word_frequencies
-    word_instances = total_word_instances
-    word_frequencies = {}
-    word_instances.each do |word, instances|
-      word_frequencies[word] = instances.to_f / @total_words
-    end
-    word_frequencies
-  end
-
-  def count_words
-    @words.length
-  end
-
-  def get_words(text)
-    text.split(" ")
-  end
-
-  def sort_by_frequency(word_frequencies)
-    sorted_frequencies = word_frequencies.sort_by { |_key, value| value }
-    descending_frequencies = sorted_frequencies.reverse
-    descending_frequencies.map { |pair| Hash[*pair] }
-  end
-
-  def total_word_instances
-    word_instances = {}
-    @words.map do |word|
-      if !word_instances.include?(word)
-        word_instances[word] = 1
+  def add_words(words)
+    words.each_index do |index|
+      first_word = words[index]
+      second_word = words[index + 1] || ""
+      if !@words.include?(first_word)
+        @words[first_word] = { count: 1, next_words: [second_word] }
       else
-        word_instances[word] += 1
+        @words[first_word][:count] += 1
+        @words[first_word][:next_words].push(second_word)
       end
     end
-    word_instances
   end
 end
