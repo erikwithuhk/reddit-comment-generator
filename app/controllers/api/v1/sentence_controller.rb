ecf80e3edd286a4f242f2891c378663b95d822ec
Erik Jönsson <efjonsson@gmail.com>
2016-10-27 16:25:33 -0400

    generate sentence with length parameter

---
+++ app/controllers/api/v1/sentence_controller.rb
@@ -0,0 +1,7 @@
+class Api::V1::SentenceController < ApplicationController
+  def index
+    length = params[:length].to_i || 10
+    new_sentence = Sentence.new(length)
+    render json: new_sentence
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :words, only: [:index, :show]
+      get "/sentence", to: "sentence#index"
     end
   end
 end
