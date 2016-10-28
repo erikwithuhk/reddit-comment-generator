a28efa58bef2cb76af240e8a62805c33510f5951
Erik Jönsson <efjonsson@gmail.com>
2016-10-28 00:14:27 -0400

    change n parameter to number

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::CommentsController < ApplicationController
   def index
-    number_of_comments = params[:n] ? params[:n].to_i : 1
+    number_of_comments = params[:number] ? params[:number].to_i : 1
     comments = get_comments(number_of_comments)
     render json: { total: number_of_comments, comments: comments }
   end
