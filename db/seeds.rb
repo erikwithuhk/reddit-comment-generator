7e81016172e55d2fee6a1a6dda12ed50973dd3c6
Erik Jönsson <efjonsson@gmail.com>
2016-10-29 13:03:42 -0400

    rename app

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,7 @@
-require 'csv'
-require './lib/word_calculator'
-
-REDDIT_SEED_FILE = "./lib/reddit_comments_limit_100_000.csv"
-
-word_calculator = WordCalculator.new
-
-lines = 0
-CSV.foreach(REDDIT_SEED_FILE) do |row|
-  word_calculator.analyze_line(row[0])
-  p row[0]
-  lines += 1
-end
-
-word_calculator.words.each do |word, data|
-  p word
-  Word.create(word: word, count: data[:count], next_words: data[:next_words])
-end
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
